a5fe41341004f951160f4138f0e705d040dd4222
dudgp128 <20181048@sungshin.ac.kr>
2022-10-12 15:39:45 +0900

    주석추가

--- hooks-tutorial/src/Nickname.js
+++ hooks-tutorial/src/Nickname.js
@@ -4,9 +4,15 @@ const Nickname = () => {
   const [name, setName] = useState("");
   const [nickname, setNickName] = useState("");
 
+  //useEffect : 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정
+
+  // 특정 값('name')이 업데이트 될 때만 실행하고 싶을때,
+  // useEffect의 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값 입력(['name'])
   useEffect(() => {
     console.log("effect");
     console.log({ name, nickname });
+    // 컴포넌트가 언마운트 되기전, 업데이트 되기 직전에 어떠한 작업을 수행하고 싶다면
+    // 뒷정리 함수를 "반환(return)" 해 주어야 함.
     return () => {
       console.log("cleanup");
       console.log({ name, nickname });
--- hooks-tutorial/src/Nickname_useReducer.js
+++ hooks-tutorial/src/Nickname_useReducer.js
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 
+// 컴포넌트 업데이트 로직을 컴포넌트 바깥으로 빼낼 수 있다.
 function reducer(state, action) {
   return {
     ...state,
@@ -8,6 +9,12 @@ function reducer(state, action) {
 }
 
 const Nickname_useReducer = () => {
+  // useReducer('reducer' , 해당 리듀서의 기본값);
+  // useReducer 이 Hook을 사용하면 state값과 dispatch 함수를 받아옴.
+  // state - 현재 가리키고 있는 상태
+  // dispatch - 액션을 발생시키는 함수로, 함수 안에 파라미터로 액션 값을 넣어주면 'reducer'함수가 호출
+  // dispathch(e.target) -> reducer(state, e.target)
+
   const [state, dispatch] = useReducer(reducer, { name: "", nickname: "" });
 
   const { name, nickname } = state;
