e618aa753df014fff15c7a016844ef77813ea518
dudgp128 <20181048@sungshin.ac.kr>
2022-12-12 20:55:11 +0900

    리덕스를사용하여 리액트 애플리케이션 상태 관리하기

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,21 +1,33 @@
+import produce from "immer";
+import { createAction, handleActions } from "redux-actions";
+
 const CHANGE_INPUT = "todos/CHANGE_INPUT";
 const INSERT = "todos/INSERT";
 const TOGGLE = "todos/TOGGLE";
 const REMOVE = "todos/REMOVE";
 
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-});
+// export const changeInput = (input) => ({
+//   type: CHANGE_INPUT,
+//   input,
+// });
 
+// let id = 3;
+// export const insert = (text) => ({
+//   type: INSERT,
+//   todo: { id: id++, text, done: false },
+// });
+
+// export const toggle = (id) => ({ type: TOGGLE, id });
+// export const remove = (id) => ({ type: REMOVE, id });
+
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
 let id = 3;
-export const insert = (text) => ({
+export const insert = createAction(INSERT, (text) => ({
   type: INSERT,
   todo: { id: id++, text, done: false },
-});
-
-export const toggle = (id) => ({ type: TOGGLE, id });
-export const remove = (id) => ({ type: REMOVE, id });
+}));
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 const initialState = {
   input: "",
@@ -33,32 +45,77 @@ const initialState = {
   ],
 };
 
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        ),
-      };
-    default:
-      return state;
-  }
-}
+// function todos(state = initialState, action) {
+//   switch (action.type) {
+//     case CHANGE_INPUT:
+//       return {
+//         ...state,
+//         input: action.input,
+//       };
+//     case INSERT:
+//       return {
+//         ...state,
+//         todos: state.todos.concat(action.todo),
+//       };
+//     case REMOVE:
+//       return {
+//         ...state,
+//         todos: state.todos.filter((todo) => todo.id !== action.id),
+//       };
+//     case TOGGLE:
+//       return {
+//         ...state,
+//         todos: state.todos.map((todo) =>
+//           todo.id === action.id ? { ...todo, done: !todo.done } : todo
+//         ),
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// const todos = handleActions(
+//   {
+//     [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+//     [INSERT]: (state, { payload: todo }) => ({
+//       ...state,
+//       todos: state.todos.concat(todo),
+//     }),
+//     [REMOVE]: (state, { payload: id }) => ({
+//       ...state,
+//       todos: state.todos.filter((todo) => todo.id !== id),
+//     }),
+//     [TOGGLE]: (state, { payload: id }) => ({
+//       ...state,
+//       todos: state.todos.map((todo) =>
+//         todo.id === id ? { ...todo, done: !todo.done } : todo
+//       ),
+//     }),
+//   },
+//   initialState
+// );
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+  },
+  initialState
+);
 export default todos;
