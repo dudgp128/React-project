70cc4bddc8b038c42174ca87d04b1b60ebc2403d
dudgp128 <20181048@sungshin.ac.kr>
2022-12-19 23:45:05 +0900

    koa-router 기본 사용법
    router.get(라우트의 경로, 해당 라우트의 적용할 미들웨어 함수)

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,27 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log('1');
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; // Unauthorized
-    return;
-  }
-  await next();
-  console.log('END');
+router.get('/', (ctx) => {
+  ctx.body = '홈';
 });
 
-app.use((ctx, next) => {
-  console.log('2');
-  next();
+router.get('/about', (ctx) => {
+  ctx.body = '소개';
 });
 
-// 서버를 포트 4000번으로 열고, 서버에 접속하면 'hello word'라는 텍스트를 반환
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listen to port 4000');
