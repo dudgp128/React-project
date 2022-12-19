881b49738790a50aadd463acd3ee584f7ec6702a
dudgp128 <20181048@sungshin.ac.kr>
2022-12-19 21:42:48 +0900

    미들웨어
    app.use(): 미들웨어 함수를 애플리케이션에 등록한다.
    미들웨어 함수 구조
    (ctx, next) => { ... }
    ctx : Context의 줄임말, 웹 요청과 응답에 대한 정보를 지님
    next : 현재 처리중인 미들웨어의 다음 미들웨어를 호출하는 함수(Promise를 반환)

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,6 +2,17 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use((ctx, next) => {
+  console.log(ctx.url);
+  console.log('1');
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log('2');
+  next();
+});
+
 // 서버를 포트 4000번으로 열고, 서버에 접속하면 'hello word'라는 텍스트를 반환
 app.use((ctx) => {
   ctx.body = 'hello world';
