58a619c90e38aa32c8834b8bd852b94651548168
dudgp128 <20181048@sungshin.ac.kr>
2022-12-19 18:19:04 +0900

    서버 띄우기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,12 @@
-const hello = 'Hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+// 서버를 포트 4000번으로 열고, 서버에 접속하면 'hello word'라는 텍스트를 반환
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('Listen to port 4000');
+});
