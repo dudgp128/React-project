3b55510892d01a9f263ac16938628cf6025339b1
dudgp128 <20181048@sungshin.ac.kr>
2022-12-19 23:57:44 +0900

    라우터의 파라미터와 쿼리

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -8,8 +8,14 @@ router.get('/', (ctx) => {
   ctx.body = '홈';
 });
 
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 });
 
 // app 인스턴스에 라우터 적용
