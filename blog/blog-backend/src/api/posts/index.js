5740f1479f2d9fe0b17831f411f3d4a457906285
dudgp128 <20181048@sungshin.ac.kr>
2022-12-20 16:06:34 +0900

    컨트롤러 함수들을 각 라우터에 연결하기

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,20 +1,12 @@
 const Router = require('koa-router');
 const posts = new Router();
+const postCtrl = require('./posts.ctrl');
 
-const printInfo = (ctx) => {
-  //현재 요청의
-  ctx.body = {
-    method: ctx.method, // 메서드
-    path: ctx.path, // 경로
-    params: ctx.params, // 파라미터
-  };
-};
-
-posts.get('/', printInfo); // (라우트의 경로, 해당 라우트에 적용할 미들웨어 함수)
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+posts.get('/', postCtrl.list); // (라우트의 경로, 해당 라우트에 적용할 미들웨어 함수)
+posts.post('/', postCtrl.write);
+posts.get('/:id', postCtrl.read);
+posts.delete('/:id', postCtrl.remove);
+posts.put('/:id', postCtrl.replace);
+posts.patch('/:id', postCtrl.update);
 
 module.exports = posts;
