ee3d04ec8ca04b865560a88926bf626a759a1373
dudgp128 <20181048@sungshin.ac.kr>
2022-12-20 16:06:00 +0900

    컨트롤러 파일 작성 : 라우터 처리 함수들을 분리하여 관리

---
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,85 @@
+let postId = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: '제목',
+    body: '내용',
+  },
+];
+
+// 포스트 작성 : POST /api/posts {title, body}
+exports.write = (ctx) => {
+  //REST API의 Request Body는 ctx.request.body에서 조회
+  const { title, body } = ctx.request.body;
+  postId += 1;
+  const post = { id: postId, title, body };
+  posts.push(post);
+  ctx.body = post;
+};
+
+// 포스트 목록 조회 : GET /api/posts
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+
+// 특정 포스트 조회 : GET /api/posts/:id
+exports.read = (ctx) => {
+  const { id } = ctx.params;
+  // id값으로 포스트를 찾는다. 파라미터로 받아온 값은 문자열이다.
+  const post = posts.find((p) => p.id.toString() === id);
+  if (!post) {
+    ctx.status = 400;
+    ctx.body = { message: '포스트가 존재하지 않습니다.' };
+    return;
+  }
+  ctx.body = post;
+};
+
+// 특정 포스트 삭제 : DELETE api/posts/:id
+exports.remove = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = { message: '포스트가 존재하지 않습니다.' };
+    return;
+  }
+  posts.splice(index, 1);
+  ctx.status = 204; // NO Content
+};
+
+// 포스트 수정(교체) : PUT /api/posts/:id {title, body}
+exports.replace = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = { message: '포스트가 존재하지 않습니다.' };
+    return;
+  }
+  // 전체 객체를 덮어 씌운다. (id를 제외한 기존 정보를 날리고, 객체를 새로 만들기)
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+
+  ctx.body = posts[index];
+};
+
+// 포스트 수정(특정 필드 변경) : PATCH /api/posts/:id {title, body}
+exports.update = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = { message: '포스트가 존재하지 않습니다.' };
+    return;
+  }
+  // 기존 값에 정보를 덮어 씌우기
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
