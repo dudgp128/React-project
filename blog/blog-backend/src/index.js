1fc6ca583689a53cd6c8afab9393331ba077ee4c
dudgp128 <20181048@sungshin.ac.kr>
2022-12-21 17:07:19 +0900

    mongoose로 서버와 DB 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,9 +2,19 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyparser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MONGO_URL } = process.env;
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.log("★",e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -20,7 +30,7 @@ app.use(bodyparser());
 app.use(router.routes()).use(router.allowedMethods());
 
 // PORT가 지정되지 않았다면, 4000을 사용
-const port = PORT || 4000;
+const port = 4000;
 app.listen(port, () => {
   console.log('Listening to port %d', port);
 });
