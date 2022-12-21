17842986e8c28b16e986529ce8186d5e4c3604a5
dudgp128 <20181048@sungshin.ac.kr>
2022-12-21 17:22:51 +0900

    esm으로 ES모듈 import/export 문법 사용하기
    esm 라이브러리 : import/export 문법은 Node.js에서 아직 정식 지원되지 않아, 해당 라이브러리의 도움을 받아 해당 문법을 사용한다.

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,36 +1,5 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyparser = require('koa-bodyparser');
-const mongoose = require('mongoose');
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다.
+/// eslint-disable no-global-assign
 
-// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MONGO_URI } = process.env;
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((e) => {
-    console.log("★",e);
-  });
-
-const app = new Koa();
-const router = new Router();
-const api = require('./api');
-
-// api 라우트 적용
-router.use('/api', api.routes()); // => /api/test
-
-// 라우터 적용 전에 bodyparser 사용하기
-app.use(bodyparser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-// PORT가 지정되지 않았다면, 4000을 사용
-const port = 4000;
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
+require=require('esm')(module /*,options*/);
+module.exports=require('/.main.js')
---
+++ blog/blog-backend/src/main.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyparser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.log("★",e);
+  });
+
+const app = new Koa();
+const router = new Router();
+const api = require('./api');
+
+// api 라우트 적용
+router.use('/api', api.routes()); // => /api/test
+
+// 라우터 적용 전에 bodyparser 사용하기
+app.use(bodyparser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// PORT가 지정되지 않았다면, 4000을 사용
+const port = 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
