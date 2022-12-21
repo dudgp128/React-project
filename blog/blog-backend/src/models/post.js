abdedc4f3b59cffbd58eebc39ea2988e60210021
dudgp128 <20181048@sungshin.ac.kr>
2022-12-21 21:16:51 +0900

    모델 생성
    모델 인스턴스를 만들고 export default를 통해 내보내주기

--- blog/blog-backend/src/models/post.js
+++ blog/blog-backend/src/models/post.js
@@ -5,9 +5,12 @@ const { Schema } = mongoose;
 const PostSchema = new Schema({
   title: String,
   body: String,
-  tages: [String],
+  tags: [String],
   publishedDate: {
     type: Date,
     default: Date.now,
   },
 });
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
