68b9f17db3c7131848c5d5e73343de64ac2196f9
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 15:17:27 +0900

    [FEAT](api/auth/auth.ctrl.js) - '로그인 상태 확인기능' 구현하기

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -92,6 +92,15 @@ export const login = async (ctx) => {
   }
 };
 
-export const check = async (ctx) => {};
+// 로그인 상태 확인
+export const check = async (ctx) => {
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님
+    ctx.state = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
+};
 
 export const logout = async (ctx) => {};
