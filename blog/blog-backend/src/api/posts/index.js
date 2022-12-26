cd4c4dab1fcf8b0d2699ed3c43a926effd24b137
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 17:30:26 +0900

    [FEAT](api/posts/index.js) - 'checkLoggedIn' 미들웨어 posts 라우터에 적용하기

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,16 +1,17 @@
 import Router from 'koa-router';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list); // (라우트의 경로, 해당 라우트에 적용할 미들웨어 함수)
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 
 post.get('/', postsCtrl.read);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
+post.delete('/', checkLoggedIn, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
