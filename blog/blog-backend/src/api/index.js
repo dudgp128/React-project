5f76bd515ff0523728b52a080594fa74b50e435a
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 13:07:20 +0900

    [FEAT](api.auth) -  회원 인증 API 틀 만들기

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,8 +1,10 @@
 import Router from 'koa-router';
+import auth from './auth';
 import posts from './posts';
 
 const api = new Router();
 
 api.use('/posts', posts.routes()); // posts 라우트 적용
+api.use('/auth', auth.routes());
 
 export default api;
