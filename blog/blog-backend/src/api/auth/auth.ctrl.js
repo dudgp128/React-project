5afdd25a1ec2be9eb63796183ab5d68b458f3912
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 13:27:50 +0900

    [FEAT](api/auth/auth.ctrl.js) - 회원가입 구현하기

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,4 +1,54 @@
-export const register = async (ctx) => {};
+// 객체 검증을 위한 라이브러리
+import Joi from '../../../node_modules/joi/lib/index';
+import User from '../../models/user';
+/*
+    POST /api/auth/register
+    {
+        username:'velopert',
+        password : 'mypass123'
+    }
+*/
+
+export const register = async (ctx) => {
+  // Request Body 검증하기
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+
+  try {
+    // username이 중복인지 확인
+    const exists = await User.findByUsername(username);
+
+    if (exists) {
+      ctx.status = 409; // Conflict
+      return;
+    }
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password); // 비밀번호 설정
+    await user.save(); // DB에 저장
+
+    // 응답할 데이터에서 hashedPassword 필드 제거
+    const data = user.toJSON();
+    delete data.hashedPassword;
+    ctx.body = data;
+    
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
 
 export const login = async (ctx) => {};
 
