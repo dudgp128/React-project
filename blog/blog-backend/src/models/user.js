8dc1b5f8d802e2615f42beabde0c9279d2baaea9
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 12:57:01 +0900

    [FEAT](models/user.js) - 비밀번호를 파라미터로 받아 계정의 hashedPassword 값을 설정하기

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -6,5 +6,11 @@ const UserSchema = new Schema({
   hashedPassword: String,
 });
 
+// 비밀번호를 파라미터로 받아 계정의 hashedPassword 값을 설정
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
 const User = mongoose.model('User', UserSchema);
 export default User;
