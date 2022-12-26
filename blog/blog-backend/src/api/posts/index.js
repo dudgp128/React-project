cf0542ff78b182b308f0221ef882ced7beded39b
dudgp128 <20181048@sungshin.ac.kr>
2022-12-26 17:25:06 +0900

    [REFACTORY](api/posts/index.js) - '/:id' 경로를 위한 라우터 새로 만들고 posts에 해당 라우터를 등록하기

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -5,8 +5,13 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list); // (라우트의 경로, 해당 라우트에 적용할 미들웨어 함수)
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+const post = new Router();
+
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
