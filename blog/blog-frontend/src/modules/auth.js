9c8d6f3f981aa76c77b15469edf515236fa9a69b
dudgp128 <20181048@sungshin.ac.kr>
2023-01-04 20:52:52 +0900

    [FEAT](modules/auth.js) - 리덕스로 폼 상태 관리하기 (input 변경시)

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,15 +1,37 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
-const SAPMLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 
-export const sampleAction = createAction(SAPMLE_ACTION);
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register, login
+    key, // username, password
+    value, // 실제 바꾸려는 값
+  }),
+);
 
-const initialState = {};
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
   {
-    [SAPMLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
   },
   initialState,
 );
+
 export default auth;
