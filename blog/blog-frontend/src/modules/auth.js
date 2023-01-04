bda536db61318975511e46fe6f28de131796002c
dudgp128 <20181048@sungshin.ac.kr>
2023-01-04 21:03:49 +0900

    [FEAT](modules/auth.js) - 리덕스로 폼 상태 관리하기 (form 초기화)

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -2,6 +2,7 @@ import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -11,6 +12,9 @@ export const changeField = createAction(
     value, // 실제 바꾸려는 값
   }),
 );
+
+export const initializeFrom = createAction(INITIALIZE_FORM, (form) => form);
+// register / login
 
 const initialState = {
   register: {
@@ -30,6 +34,10 @@ const auth = handleActions(
       produce(state, (draft) => {
         draft[form][key] = value;
       }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState,
 );
