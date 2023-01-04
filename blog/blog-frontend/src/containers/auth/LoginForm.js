f713cd81545acb3f685693fe3f8df47d0d235eda
dudgp128 <20181048@sungshin.ac.kr>
2023-01-04 22:04:25 +0900

    [FIXED] - 오타수정

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -4,9 +4,10 @@ import { changeField, initializeFrom } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const LoginForm = () => {
-  const form = useSelector(({ auth }) => ({
+  const { form } = useSelector(({ auth }) => ({
     form: auth.login,
   }));
+
   const dispatch = useDispatch();
 
   // input 변경 이벤트 핸들러
@@ -22,9 +23,9 @@ const LoginForm = () => {
   };
 
   //컴포넌트가 처음 렌더링 될 때, form을 초기화함.
-  useEffect(() => dispatch(initializeFrom(form)), [dispatch]);
+  useEffect(() => dispatch(initializeFrom('login')), [dispatch]);
 
-  return <AuthForm type="login" form={form} onChangeField={onChange} />;
+  return <AuthForm type="login" form={form} onChange={onChange} />;
 };
 
 export default LoginForm;
