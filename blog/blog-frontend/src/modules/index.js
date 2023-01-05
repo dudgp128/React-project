13974b4fa78ef1357ab17df80586dc5037e43ddb
dudgp128 <20181048@sungshin.ac.kr>
2023-01-05 14:31:35 +0900

    [FEAT](modules/loading.js) - redux-saga를 통해 더 쉽게 API를 요청하기

--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import auth from './auth';
+import auth from '../api/auth/index';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   auth,
+  loading,
 });
 
 export default rootReducer;
