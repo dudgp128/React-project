f49b3002cfc64799ae46264e5aa497a593428607
dudgp128 <20181048@sungshin.ac.kr>
2023-01-08 15:58:00 +0900

    [FEAT](containers/auth/RegisterForm.js) - 회원가입 구현하기 : 폼 등록 이벤트 핸들러 만들기

--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
-import { initializeForm, changeField } from '../../modules/auth';
+import { initializeForm, changeField, register } from '../../modules/auth';
 
 const RegisterForm = () => {
-  const { form } = useSelector(({ auth }) => ({ form: auth.register }));
+  // useSelector : connect 대신 redux 상태 조회 가능
+  const { form, auth, authError } = useSelector(({ auth }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+  }));
+
+  // useDispatch : 컴포넌트에서 스토어의 내장 함수 dispatch 사용
   const dispatch = useDispatch();
 
   const onChange = (e) => {
@@ -16,6 +23,18 @@ const RegisterForm = () => {
         value,
       }),
     );
+  };
+
+  // 폼 등록 이벤트 핸들러
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const { username, password, passwordConfirm } = form; // == register
+    if (password !== passwordConfirm) {
+      // Todo : 오류 처리
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   useEffect(() => dispatch(initializeForm('register')), [dispatch]);
