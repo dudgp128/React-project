882b0f4a8ffc5044bced20e8d261b5dd31c07967
dudgp128 <20181048@sungshin.ac.kr>
2023-01-08 14:40:31 +0900

    [FIXED]react-saga를 사용하여 API 요청 상태 관리하기

---
+++ learn-redux-middleware/src/lib/createRequestSaga.js
@@ -0,0 +1,26 @@
+import { call, put } from "redux-saga/effects";
+import { startLoading, finishLoading } from "../modules/loading";
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  // id 처럼 요청에 필요한 값을 액션의 payload로 넣어주여야한다.
+  return function* (action) {
+    yield put(startLoading(type));
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (error) {
+      yield put({
+        type: FAILURE,
+        payload: error,
+        error: true,
+      });
+    }
+    yield put(finishLoading(type));
+  };
+}
