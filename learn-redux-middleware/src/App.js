057f4d705f9b2ccbc0a0405251582d7515a3bcde
dudgp128 <20181048@sungshin.ac.kr>
2023-01-08 14:54:32 +0900

    [FEAT] (modules/index.js) - sampleSaga를 rootSaga에 등록하기

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,9 +1,9 @@
-import CounterContainer from "./container/CounterContainer";
+import SampleContainer from "./container/SampleContainer";
 
 const App = () => {
   return (
     <div>
-      <CounterContainer />
+      <SampleContainer />
     </div>
   );
 };
--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import counter, { counterSaga } from "./counter";
-import sample from "./sample";
+import sample, { sampleSaga } from "./sample";
 import loading from "./loading";
 
 const rootReducer = combineReducers({
@@ -11,7 +11,9 @@ const rootReducer = combineReducers({
 });
 
 export function* rootSaga() {
-  yield all([counterSaga()]);
+// all 함수를 사용해, 제너레이터 함수를 배열로 넣으면
+// 제너레이터 함수들이 동시 실행, 전부 resolve 될 때까지 기다림.
+  yield all([counterSaga(), sampleSaga()]);
 }
 
 export default rootReducer;
