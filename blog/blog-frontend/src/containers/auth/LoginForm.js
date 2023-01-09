90c34dcebaf1ca33830357474eefc5640ac9cd76
dudgp128 <20181048@sungshin.ac.kr>
2023-01-09 18:20:24 +0900

    [FEAT](containers/auth/LoginForm.js) - 폼 등록 이벤트 핸들러 만들기

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const LoginForm = () => {
@@ -25,12 +25,25 @@ const LoginForm = () => {
     );
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
+  };
+
   //컴포넌트가 처음 렌더링 될 때, form을 초기화함.
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
 
-  return <AuthForm type="login" form={form} onChange={onChange} />;
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default LoginForm;
