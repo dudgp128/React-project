ac62752b5aadce9738b5ee90542720b5bd3d1bd5
dudgp128 <20181048@sungshin.ac.kr>
2023-02-02 14:03:24 +0900

    [FEAT](lib/api/posts.js) - readPost : API를 연동해 포스트를 읽게 하기

--- blog/blog-frontend/src/lib/api/posts.js
+++ blog/blog-frontend/src/lib/api/posts.js
@@ -3,3 +3,6 @@ import client from './client';
 // post 글쓰기
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+// post 조회하기
+export const readPost = (id) => client.get(`api/posts/${id}`);
